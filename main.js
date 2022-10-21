75cd0d6f09bd83fab5d71fb04f22091d6ecbc22f
e-v-pavlova <catheripa@gmail.com>
2022-10-21 14:44:09 +0100

    improve styles

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
+//control opening paragraphs when clicking on their headings
+
 let titles = document.querySelectorAll(".paragraph__title");
 
 for (let i = 0; i < titles.length; i++) {
     titles[i].addEventListener("click", function() {
         let sibling = this.nextElementSibling;
-        console.log(sibling)
         if (sibling.classList.contains("paragraph__content")) {
-            console.log("hi2")
             sibling.classList.toggle("paragraph__content_closed");
         }
     });
 }
+
+
+//control opening paragraphs when clicking on tool buttons
 
 let buttonOpener = document.querySelector(".tool__button-opener");
 let buttonCloser = document.querySelector(".tool__button-closer");
@@ -26,3 +29,15 @@ buttonCloser.addEventListener("click", () => {
         content.classList.add("paragraph__content_closed");
     })
 });
+
+
+//control style of tool element when page scroll
+
+let toolElement = document.getElementById('tool');
+window.onscroll = function () {
+    if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100 ) {
+        toolElement.classList.add("tool-colored");
+    } else {
+        toolElement.classList.remove("tool-colored");
+    }
+};
